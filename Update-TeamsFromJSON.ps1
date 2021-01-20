50ee6fbc9570d8761836240a0da8e07f842fffde
Ivan Porta <porta.ivan@outlook.com>
2021-01-20 15:31:43 +0000

    Update Update-TeamsFromJSON.ps1

--- Update-TeamsFromJSON.ps1
+++ Update-TeamsFromJSON.ps1
@@ -7,25 +7,25 @@ Param(
     [string]$TeamsFilePath
 )
 
-Write-Verbose "Import modules"
+Write-Verbose "Importing modules"
 $Module = Get-Module -Name MicrosoftTeams -ListAvailable 
 if($Module.Count -eq 0) {   
-  Write-Verbose "Install MicrosoftTeams module"
+  Write-Verbose "Installing MicrosoftTeams module"
   Install-Module -Name MicrosoftTeams -AllowPrerelease -AllowClobber -Force
 }
 
 # Functions
 Function New-MicrosoftTeam([object]$Team) {
   Try {
-    Write-Verbose "Create $($Team.DisplayName) $($Team.Visibility) team"
+    Write-Verbose "Creating $($Team.DisplayName) $($Team.Visibility) team"
     $NewTeam = New-Team -DisplayName $Team.DisplayName -Visibility $Team.Visibility
-
-    Write-Verbose "Add $($Team.Users.Length) users to $($Team.DisplayName) team"
+    Write-Verbose "Adding $($Team.Users.Length) users to $($Team.DisplayName) team"
     $Team.Users | ForEach-Object -Begin {
       $Index = 0   
     } -Process {
       $Index = $Index + 1
       Write-Progress -Id 1 -ParentId 0 -Activity "Add user to the team" -Status "$($Index) of $($Team.Users.Length) - User: $($_.Email), Role: $($_.Role)" -PercentComplete ($Index/$Team.Users.Length*100)
+      Write-Verbose "Adding $($_.Email) to $($Team.DisplayName) teams as $($_.Role)"
       Add-TeamUser -User $_.Email -Role $_.Role -GroupId $NewTeam.GroupId
     } -End {
       Write-Verbose "Users succesfully added to the $($Team.DisplayName) team"
@@ -37,7 +37,7 @@ Function New-MicrosoftTeam([object]$Team) {
     } -Process {
       $Index = $Index + 1
       Write-Progress -Id 2 -ParentId 0 -Activity "Creation of a new channel" -Status "$($Index) of $($Team.Channels.Length) - Display Name: $($_.DisplayName), Membership Type: $($_.MembershipType)" -PercentComplete ($index/$Team.Channels.Length*100)   
-      New-TeamChannel -DisplayName $_.FisplayName -MembershipType $_.MembershipType -GroupId $NewTeam.GroupId
+      New-TeamChannel -DisplayName $_.DisplayName -MembershipType $_.MembershipType -GroupId $NewTeam.GroupId
 
       Write-Verbose "Check channel membership type"
       if('Private' -eq $_.MembershipType -And $_.Users.Length -gt 0) {
@@ -49,6 +49,7 @@ Function New-MicrosoftTeam([object]$Team) {
         } -Process {
           $IndexUsers = $IndexUsers + 1
           Write-Progress -Id 3 -ParentId 2 -Activity "Add user to the private channel" -Status "$($IndexUsers) of $($UsersLength) - User: $($_.Email), Role: $($_.Role)" -PercentComplete ($IndexUsers/$UsersLength*100)   
+          Write-Verbose "Adding $($_.Email) to $($DisplayName) private channel as $($_.Role)"
           Add-UserToPrivateChannel -DisplayName $DisplayName -Email $_.Email -Role $_.Role -GroupId $NewTeam.groupId
         } -End {
           Write-Verbose "Users succesfully added to the $($_.DisplayName) channel"
@@ -70,7 +71,7 @@ Function Add-UserToPrivateChannel([string]$DisplayName, [string]$Email, [string]
       Write-Verbose "$($Attemps) attempt/s"
       Write-Verbose "Waiting $(60*$Attemps) seconds"
       Start-Sleep -s (60*$Attemps)
-      Write-Verbose "Try to add $($Email) to $($DisplayName) private channel"
+      Write-Verbose "Adding $($Email) to $($DisplayName) private channel"
       Add-TeamChannelUser -DisplayName $DisplayName -User $Email -GroupId $GroupId
       Write-Verbose "Check user role"
       if("Owner" -eq $Role){     
@@ -87,12 +88,14 @@ Function Add-UserToPrivateChannel([string]$DisplayName, [string]$Email, [string]
   } while ($Attemps -lt $MaxNumberOfAttemps)
 }
 
-Write-Verbose "Connect to MS Teams"
+Write-Verbose "Generating secure password"
 $SecurePassword = ConvertTo-SecureString -AsPlainText $Office365Password -Force
+Write-Verbose "Generating PSCredential object"
 $Credentials = New-Object -TypeName System.Management.Automation.PSCredential -Argumentlist $Office365Username, $SecurePassword;
+Write-Verbose "Connecting to Microsoft Teams"
 Connect-MicrosoftTeams -Credential $Credentials
 
-Write-Verbose "Read JSON file"
+Write-Verbose "Read JSON file from $($TeamsFilePath)"
 $Json = Get-Content -Raw -Path $TeamsFilePath | ConvertFrom-Json
 $Json.Teams | ForEach-Object -Begin {
   $Index = 0    
